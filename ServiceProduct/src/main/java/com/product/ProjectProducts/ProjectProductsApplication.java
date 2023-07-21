fdeda527851980165b752f14f5aba934f2bf13c4
Luis Florez <braian1k3@gmail.com>
2023-07-21 10:55:33 -0500

    New service with Spring Initializr for "Product".

---
+++ ServiceProduct/src/main/java/com/product/ProjectProducts/ProjectProductsApplication.java
@@ -0,0 +1,13 @@
+package com.product.ProjectProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectProductsApplication.class, args);
+	}
+
+}
---
+++ ServiceProduct/src/test/java/com/product/ProjectProducts/ProjectProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.product.ProjectProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
